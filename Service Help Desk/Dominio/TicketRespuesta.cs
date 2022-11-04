b3857c7ba696cd33adc3a23303da4736f0f4eed3
Juan Cruz <Juan Cruz@JUANCRUZW>
2022-11-04 17:40:47 -0300

    Se agregan paginas

---
+++ Service Help Desk/Dominio/TicketRespuesta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TicketRespuesta
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int TicketId { get; set; }
+        public string Respuesta { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Estado { get; set; }
+    }
+}
--- Service Help Desk/SQL/stores_procedures.sql
+++ Service Help Desk/SQL/stores_procedures.sql
@@ -5,3 +5,8 @@ CREATE PROCEDURE sp_obtenerCategorias
 AS
 SELECT ID, NOMBRE FROM Categorias
 GO;
+
+CREATE PROCEDURE sp_obtenerPrioridades
+AS
+SELECT ID, NOMBRE FROM Prioridades
+GO;
